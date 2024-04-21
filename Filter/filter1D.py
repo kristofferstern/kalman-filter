Fixed a sign error.

Filter/filter1D.py
@@ -97,7 +97,7 @@ class FadingMemoryFilter1D:
         K = (np.matmul(P_1, np.transpose(C))) * (1.0/(P_1[0,0] + R))
 
         #### Update state estimate
-        self.__state_estimate = x_1 - K * (measurement - x_1[0,0])
+        self.__state_estimate = x_1 + K * (measurement - x_1[0,0])
 
         #### Update uncertainty estimate 
         self.__state_covariance_estimate = P_1 - np.matmul(K, np.transpose(K)) * (P_1[0,0] + R)
@@ -169,9 +169,6 @@ class SimpleFilter1D(IFilter):
         valid = state is not None
         
         if self.__requirements_in_points:
-            bob = self.__cummulative_points
-            john = self.__min_points
-            cake = self.__input_times
             valid = valid and self.__cummulative_points >= self.__min_points and len(self.__input_times) >= self.__min_points
         else:
             if len(self.__input_times) == 0:

main.py
@@ -29,7 +29,7 @@ time = np.linspace(0, total_time, int(total_time*(1/time_res)))
 time_res = time[1] - time[0]
 
 pos = np.zeros(time.size)
-bob = np.zeros(time.size)
+pos_est = np.zeros(time.size)
 for i in range(time.size):
     device.UpdateState(time[i])
     pos[i] = device.getMeasurement()
@@ -37,10 +37,10 @@ for i in range(time.size):
         filter.AddMeasurement(0, pos[i])
     else:
         filter.AddMeasurement(time_res, pos[i])
-    john = filter.GetFitResult(0)
-    if john is not None:
-        bob[i] = john[0]
+    est = filter.GetFitResult(0)
+    if est is not None:
+        pos_est[i] = est[0]
 
 plt.plot(time, pos)
-plt.plot(time, bob)
+plt.plot(time, pos_est)
 plt.show()